8a570c4c63efb2b162dbe449bdfb04dbb6789f7b
mjkuranda <kuranda.marek@gmail.com>
2023-08-26 14:13:15 +0200

    register and login

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,47 +1,17 @@
-import { Body, Controller, Post, Headers, Response } from '@nestjs/common';
+import { Body, Controller, Post, Response } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto, UserLoginDto } from './user.dto';
-import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
 import { QueryResult } from '../../common/interfaces';
+import { UserDocument } from './user.interface';
 
 @Controller('users')
 export class UserController {
 
-    constructor(private readonly userService: UserService,
-                private readonly jwtService: JwtService) {}
+    constructor(private readonly userService: UserService) {}
 
     @Post('/login')
-    public async login(
-        @Body() loginBody: UserLoginDto,
-        @Headers() headers,
-        @Response({ passthrough: true }) res
-    ): Promise<QueryResult<{ login: string; password: string; }>> {
-        const user = (await this.userService.getUsers()).find(user => user.login === loginBody.login);
-
-        if (!user) {
-            return {
-                message: 'User does not exist',
-                statusCode: 400
-            };
-        }
-
-        if (!await bcrypt.compare(loginBody.password, user.password)) {
-            return {
-                message: 'Incorrect credentials',
-                statusCode: 400
-            }
-        }
-
-        const jwt = await this.jwtService.signAsync(loginBody);
-
-        res.cookie('jwt', jwt, { httpOnly: true });
-
-        return {
-            data: user,
-            message: 'Successfully logged in',
-            statusCode: 200
-        }
+    public async login(@Body() loginBody: UserLoginDto, @Response({ passthrough: true }) res): Promise<QueryResult<UserDocument>> {
+        return await this.userService.loginUser(loginBody, res);
     }
 
     @Post('/create')
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,20 +1,54 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './user.dto';
+import { CreateUserDto, UserLoginDto } from './user.dto';
 import * as bcrypt from 'bcrypt';
-import {QueryResult} from "../../common/interfaces";
-import {UserDocument} from "./user.interface";
+import { QueryResult } from '../../common/interfaces';
+import { UserDocument } from './user.interface';
+import { JwtService } from '@nestjs/jwt';
 
 const users = [];
 
 @Injectable()
 export class UserService {
 
+    constructor(private readonly jwtService: JwtService) {}
+
     async getUsers(): Promise<UserDocument[]> {
         return Promise.resolve(users);
     }
 
-    async loginUser(): Promise<QueryResult<UserDocument>> {
+    async getUser(login: string): Promise<UserDocument | null> {
+        const users = await this.getUsers();
 
+        return users.find(user => user.login === login);
+    }
+
+    async loginUser(userLoginDto: UserLoginDto, res): Promise<QueryResult<UserDocument>> {
+        const { login, password } = userLoginDto;
+        const user = await this.getUser(login);
+
+        if (!user) {
+            return {
+                message: 'User does not exist',
+                statusCode: 400
+            };
+        }
+
+        if (!await bcrypt.compare(password, user.password)) {
+            return {
+                message: 'Incorrect credentials',
+                statusCode: 400
+            }
+        }
+
+        const jwt = await this.jwtService.signAsync(login);
+
+        res.cookie('jwt', jwt, { httpOnly: true });
+
+        return {
+            data: user,
+            message: 'Successfully logged in',
+            statusCode: 200
+        }
     }
 
     async createUser(createUserDto: CreateUserDto): Promise<QueryResult<UserDocument>> {
