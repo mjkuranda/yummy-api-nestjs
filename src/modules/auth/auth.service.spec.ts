e67fb8d63ea3c725b6b3bcf7d53b0fac09434c51
mjkuranda <kuranda.marek@gmail.com>
2023-09-05 14:26:20 +0200

    refactor

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,10 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import { getModelToken, MongooseModule } from '@nestjs/mongoose';
+import { getModelToken } from '@nestjs/mongoose';
 import { models } from '../../constants/models.constant';
-import { UserSchema } from '../user/user.schema';
-import { UserModule } from '../user/user.module';
-import { JwtManagerModule } from '../jwt-manager/jwt-manager.module';
 import { connect, Connection, Model } from 'mongoose';
 import { UserDocument } from '../user/user.interface';
 import { JwtManagerService } from '../jwt-manager/jwt-manager.service';
@@ -27,13 +24,8 @@ describe('AuthService', () => {
         mongoConnection = (await connect(uri)).connection;
 
         const module: TestingModule = await Test.createTestingModule({
-            // imports: [
-            //     UserModule,
-            //     JwtManagerModule
-            // ],
             providers: [
                 AuthService,
-                // LoggerService,
                 {
                     provide: JwtManagerService,
                     useValue: {
