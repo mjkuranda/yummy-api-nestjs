38372c7da015542b67ed11beff663e43ecc67be4
mjkuranda <kuranda.marek@gmail.com>
2023-09-18 13:59:49 +0200

    prepare for next tests

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,20 +1,13 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { UserModule } from '../src/modules/user/user.module';
 import { UserService } from '../src/modules/user/user.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { models } from '../src/constants/models.constant';
-import { JwtService } from '@nestjs/jwt';
 import { JwtManagerService } from '../src/modules/jwt-manager/jwt-manager.service';
-import { LoggerService } from '../src/modules/logger/logger.service';
-import { REDIS_CLIENT } from '../src/modules/redis/redis.constants';
-import { RedisService } from '../src/modules/redis/redis.service';
-import { LoggerModule } from '../src/modules/logger/logger.module';
 import { AuthService } from '../src/modules/auth/auth.service';
-import { AuthModule } from '../src/modules/auth/auth.module';
 import { AppModule } from '../src/app.module';
-import { Model, Types } from 'mongoose';
+import { Model } from 'mongoose';
 import { UserDocument } from '../src/modules/user/user.interface';
 import * as cookieParser from 'cookie-parser';
 
@@ -38,53 +31,8 @@ describe('UserController (e2e)', () => {
             ? cookies[cookieName]
             : undefined;
     };
-    const mockUserService = {
-        findOne: jest.fn(),
-        getUser: jest.fn(),
-        areSameHashedPasswords: jest.fn(),
-        create: jest.fn().mockImplementation((user) => ({
-            ...user,
-            _id: '123-abc',
-            password: 'hashed'
-        })),
-        updateOne: jest.fn()
-    };
 
     beforeEach(async () => {
-        // const moduleFixture: TestingModule = await Test.createTestingModule({
-        //     imports: [UserModule, LoggerModule, AuthModule],
-        //     providers: [
-        //         UserService,
-        //         {
-        //             provide: getModelToken(models.USER_MODEL),
-        //             useValue: mockUserService
-        //         },
-        //         {
-        //             provide: JwtService,
-        //             useClass: JwtService
-        //         },
-        //         {
-        //             provide: JwtManagerService,
-        //             useClass: JwtManagerService
-        //         },
-        //         {
-        //             provide: LoggerService,
-        //             useValue: {
-        //                 info: () => {},
-        //                 error: () => {}
-        //             }
-        //         },
-        //         {
-        //             provide: REDIS_CLIENT,
-        //             useValue: {}
-        //         }
-        //     ]
-        // })
-        //     .overrideProvider(AuthService).useValue({ getAuthorizedUser: jest.fn() })
-        //     .overrideProvider(LoggerService).useValue({ info: jest.fn(), error: jest.fn() })
-        //     .overrideProvider(RedisService).useValue({})
-        //     .overrideProvider(getModelToken(models.USER_MODEL)).useValue(mockUserService)
-        //     .compile();
         const moduleRef = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
@@ -93,9 +41,6 @@ describe('UserController (e2e)', () => {
         app.use(cookieParser());
         await app.init();
 
-        // app = moduleFixture.createNestApplication();
-        // await app.init();
-        //
         userService = moduleRef.get(UserService);
         jwtManagerService = moduleRef.get(JwtManagerService);
         authService = moduleRef.get(AuthService);
@@ -165,19 +110,6 @@ describe('UserController (e2e)', () => {
     });
 
     describe('/users/:login/grant/:capability (POST)', () => {
-        // it ('should fail when user does not exist', () => {
-        //     const mockRequestLogin = 'USER';
-        //     const mockRequestCapability = 'canAdd';
-        //
-        //     return request(app.getHttpServer())
-        //         .post(`/users/${mockRequestLogin}/grant/${mockRequestCapability}`)
-        //         .set('Cookie', ['jwt=token'])
-        //         .expect(404)
-        //         .then(res => {
-        //             expect(res.body.message).toBe('Failed action to grant a permission. User with provided login does not exist.');
-        //         });
-        // });
-
         it ('should grant a new permission when user is admin', () => {
             const mockRequestLogin = 'USER';
             const mockRequestCapability = 'canAdd';
@@ -205,5 +137,11 @@ describe('UserController (e2e)', () => {
                 .expect(expectedStatusCode)
                 .expect(expectedResult);
         });
+
+        // false when user already owns this permission
+
+        // throw ForbiddenException when user is not an admin
+
+        // throw NotFoundException when user does not exist
     });
 });
