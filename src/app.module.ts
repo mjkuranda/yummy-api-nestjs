640bc77580fbe1a4284d5124b98ff77659bd80e3
mkuranda <kuranda.marek@gmail.com>
2024-02-15 11:50:17 +0100

    Removed `AuthModule` and middleware classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getMongooseUri } from './utils';
 import { IngredientModule } from './modules/ingredient/ingredient.module';
 import { UserModule } from './modules/user/user.module';
-import { AuthorizeMiddleware } from './middleware/authorize.middleware';
-import { AuthModule } from './modules/auth/auth.module';
 import { LoggerModule } from './modules/logger/logger.module';
-import { AuthenticateUserMiddleware } from './middleware/authenticate-user.middleware';
-import { AuthorizeUserMiddleware } from './middleware/authorize-user-middleware';
-import { AuthorizeUserActionMiddleware } from './middleware/authorize-user-action.middleware';
 
 @Module({
     imports: [
@@ -27,8 +22,7 @@ import { AuthorizeUserActionMiddleware } from './middleware/authorize-user-actio
         LoggerModule,
         IngredientModule,
         MealModule,
-        UserModule,
-        AuthModule
+        UserModule
     ],
     controllers: [],
     providers: [],
@@ -36,35 +30,35 @@ import { AuthorizeUserActionMiddleware } from './middleware/authorize-user-actio
 export class AppModule implements NestModule {
 
     configure(consumer: MiddlewareConsumer) {
-        consumer
-            .apply(AuthorizeMiddleware)
-            .forRoutes(
-                // { path: '/meals/create', method: RequestMethod.POST },
-                // { path: '/meals/:id', method: RequestMethod.PUT },
-                // { path: '/meals/:id', method: RequestMethod.DELETE },
-                // { path: '/ingredients/create', method: RequestMethod.POST }
-            );
-        consumer
-            .apply(AuthenticateUserMiddleware)
-            .forRoutes(
-                // { path: '/users/:login/grant/:capability', method: RequestMethod.POST },
-                // { path: '/users/:login/deny/:capability', method: RequestMethod.POST },
-                // { path: '/meals/:id/create', method: RequestMethod.POST },
-                // { path: '/meals/:id/edit', method: RequestMethod.POST },
-                // { path: '/meals/:id/delete', method: RequestMethod.POST }
-            );
-        consumer
-            .apply(AuthorizeUserMiddleware)
-            .forRoutes(
-                // { path: '/users/:login/grant/:capability', method: RequestMethod.POST },
-                // { path: '/users/:login/deny/:capability', method: RequestMethod.POST }
-            );
-        consumer
-            .apply(AuthorizeUserActionMiddleware)
-            .forRoutes(
-                // { path: '/meals/:id/create', method: RequestMethod.POST },
-                // { path: '/meals/:id/edit', method: RequestMethod.POST },
-                // { path: '/meals/:id/delete', method: RequestMethod.POST }
-            );
+    //     consumer
+    //         .apply(AuthorizeMiddleware)
+    //         .forRoutes(
+    //             { path: '/meals/create', method: RequestMethod.POST },
+    //             { path: '/meals/:id', method: RequestMethod.PUT },
+    //             { path: '/meals/:id', method: RequestMethod.DELETE },
+    //             { path: '/ingredients/create', method: RequestMethod.POST }
+    //         );
+    //     consumer
+    //         .apply(AuthenticateUserMiddleware)
+    //         .forRoutes(
+    //             { path: '/users/:login/grant/:capability', method: RequestMethod.POST },
+    //             { path: '/users/:login/deny/:capability', method: RequestMethod.POST },
+    //             { path: '/meals/:id/create', method: RequestMethod.POST },
+    //             { path: '/meals/:id/edit', method: RequestMethod.POST },
+    //             { path: '/meals/:id/delete', method: RequestMethod.POST }
+    //         );
+    //     consumer
+    //         .apply(AuthorizeUserMiddleware)
+    //         .forRoutes(
+    //             { path: '/users/:login/grant/:capability', method: RequestMethod.POST },
+    //             { path: '/users/:login/deny/:capability', method: RequestMethod.POST }
+    //         );
+    //     consumer
+    //         .apply(AuthorizeUserActionMiddleware)
+    //         .forRoutes(
+    //             { path: '/meals/:id/create', method: RequestMethod.POST },
+    //             { path: '/meals/:id/edit', method: RequestMethod.POST },
+    //             { path: '/meals/:id/delete', method: RequestMethod.POST }
+    //         );
     }
 }
