d3bbe1e60923aa49595c1f7d715d553b7aa9647d
Marek Kurańda <89488013+mjkuranda@users.noreply.github.com>
2024-07-10 19:27:21 +0200

    [1.30.0] Enabled CORS for `http://localhost:3000/` (#38)

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { ValidationPipe } from './pipes/validation.pipe';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: ['http://localhost:3000'],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        credentials: true,
+    });
     app.use(cookieParser());
     app.useGlobalFilters(new HttpExceptionFilter());
     app.useGlobalPipes(new ValidationPipe());
