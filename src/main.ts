a59288cd025fd91840c103e79da3bd060c8f77f2
mkuranda <kuranda.marek@gmail.com>
2025-02-22 09:55:55 +0100

    add interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,22 @@ import cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ValidationPipe } from './pipes/validation.pipe';
 import { getCorsOrigins } from './utils';
+import { CorsInterceptor } from './interceptors/cors.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
     app.enableCors({
         origin: getCorsOrigins(),
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
         credentials: true
     });
+
     app.use(cookieParser());
     app.useGlobalFilters(new HttpExceptionFilter());
     app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalInterceptors(new CorsInterceptor());
+
     await app.listen(3001);
 }
 
