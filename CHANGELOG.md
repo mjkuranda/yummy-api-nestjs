e0fc97e0e26361724843de623736210c129299e9
Marek Kurańda <89488013+mjkuranda@users.noreply.github.com>
2025-02-24 16:21:17 +0100

    [2.13.0] `GET /healthcheck` route (#104)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.13.0] - 2025-02-24
+### Added
+- [Marek Kurańda](https://github.com/mjkuranda): `GET /healthcheck` endpoint for checking API activity.
+
 ## [2.12.0] - 2025-02-22
 ### Added
 - [Marek Kurańda](https://github.com/mjkuranda): `CorsInterceptor` to block requests from another source.
---
+++ src/modules/healthcheck/healthcheck.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, HttpCode, Options, Res } from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller('healthcheck')
+export class HealthcheckController {
+
+    @Get()
+    @HttpCode(200)
+    public check() {
+        return {
+            message: 'Application is responding.'
+        };
+    }
+
+    @Options()
+    public handlePreflightOptionsCheck(@Res() res: Response): void {
+        res.header('Access-Control-Allow-Origin', '*'); // Zezwalamy na dowolny origin
+        res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        res.sendStatus(204);
+    }
+}
